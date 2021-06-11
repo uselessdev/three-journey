310a4d99e72aef4b63e15ad6658292ffc001eae0
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2021-06-11 01:13:31 -0300

    adicionando o react-spring

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,11 @@ import { Box } from './components/box'
 function App() {
   return (
     <div id="canvas-container">
-      <Canvas camera={{ fov: 75, position: [0, 0, 3], far: 75 }}>
-        <group position={[0, 0.25, 0]} rotation={[0, 1, 0]}>
-          <Box color={0xff0000} position={[-2, 0, 0]} />
-          <Box color={0x00ff00} position={[0, 0, 0]} />
-          <Box color={0x0000ff} position={[2, 0, 0]} />
-        </group>
+      <Canvas camera={{ fov: 75, position: [-3, 3, 3] }}>
+        <Box color="royalblue" position={[0, 0, 0]} />
+
+        <ambientLight intensity={0.1} />
+        <directionalLight castShadow />
       </Canvas>
     </div>
   )
--- src/components/box.tsx
+++ src/components/box.tsx
@@ -1,14 +1,22 @@
 import { MeshProps, Color } from "@react-three/fiber";
+import { useRef, useState } from "react";
+import { useSpring, animated, config } from '@react-spring/three'
 
 interface Props extends MeshProps {
   color: Color
 }
 
 export function Box ({ color, ...props }: Props) {
+  const [status, setStatus] = useState(0)
+  const mesh = useRef<MeshProps>()
+  const { spring } = useSpring({ spring: status, config: config.wobbly })
+
+  const rotation = spring.to([0, 1], [0, Math.PI])
+
   return (
-    <mesh {...props}>
+    <animated.mesh position-x={rotation} {...props} ref={mesh} onClick={() => setStatus(Number(!status))}>
       <boxGeometry args={[1, 1, 1]} />
-      <meshBasicMaterial color={color} />
-    </mesh>
+      <meshStandardMaterial color={color} />
+    </animated.mesh>
   )
 }
